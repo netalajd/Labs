06e4a09f05ecd18049a3490ec98ce30d144b8863
netalajd <89546945+netalajd@users.noreply.github.com>
2021-08-25 15:31:55 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*********************************************************************
+*Java Program Name
+**********************************************************************
+* Jonathan Netala
+* 8/25/2021
+* CMSC 255 Section 004
+**********************************************************************/
+
+public class Hello {
+	public static void main (String [] args) {
+		
+		System.out.println("<Jonathan Netala>");
+		System.out.println("<>");
+		System.out.println("<Program/Project Description>"); 
+		System.out.println("<8/25/2021>");
+		System.out.println("<CMSC 255 Section 004>");
+		System.out.println(); 
+		
+	}
+}
